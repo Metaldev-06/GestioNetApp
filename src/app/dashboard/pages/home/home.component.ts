5ef8d8ae6c0749e5d90907a01f8ffcb1bfb54f2b
Fernando Diaz <fernydiaz62@gmail.com>
2025-03-06 02:41:37 -0300

    Agregar diseño y funcionalidad para la visualización de clientes en el componente de inicio

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { CustomerService } from '../../../core/services/customer.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { Customer } from '../../../core/interfaces/customers-response.interface';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export default class HomeComponent {}
+export default class HomeComponent implements OnInit {
+  public customers = signal<Customer[]>([]);
+
+  private readonly customerService = inject(CustomerService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.getCustomers();
+  }
+
+  getCustomers() {
+    this.customerService
+      .getAllCustomers()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (customers) => {
+          this.customers.set(customers.data);
+        },
+        error: (error) => {},
+      });
+  }
+}
